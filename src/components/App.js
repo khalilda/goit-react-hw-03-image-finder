6f74c8acd7b10a940de6738d89c03c9adeb8cd80
khalilda <100872154+khalilda@users.noreply.github.com>
2023-08-19 23:48:03 +0300

    hw3

--- src/components/App.js
+++ src/components/App.js
@@ -16,34 +16,61 @@ import { fetchHitsByQuery } from './services/api';
 
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      images: [],
-      query: '',
-      page: 1,
-      isLoading: false,
-      showBtn: false,
-      showModal: false,
-      largeImageURL: '',
-      error: null,
-    };
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-    this.setState({
-      query: e.target.search.value,
-      isLoading: true,
-      images: [],
-    });
-    this.fetchGallery(e.target.search.value, this.state.page);
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+    loading: false,
   };
+
+
+  changeQuery = newQuery => {
+    this.setState({
+      query: `${Date.now()}/${newQuery}`,
+      images: [],
+      page: 1
+    });
+    
+  };
+
+
+  componentDidUpdate = async (prevProps, prevState) => {
+    console.log('componentDidUpdate called');
+  
+    const prevQuery = prevState.query;
+    const searchQuery = this.state.query;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+  
+    if (prevQuery !== searchQuery || prevPage !== nextPage) {
+      console.log('Fetching new data');
+      this.fetchGallery(this.state.query, this.state.page);
+    }
+  };
+  // componentDidUpdate = async (prevProps, prevState) => {
+  //   const prevQuery = prevState.query;
+  //   const searchQuery = this.state.query;
+  //   const prevPage = prevState.page;
+  //   const nexPage = this.state.page;
+
+  //   if (prevQuery !== searchQuery || prevPage !== nexPage) {
+  //     this.loadResult();
+  //   }
+  // };
+
+  // onSubmit = e => {
+  //   e.preventDefault();
+  //   this.setState({
+  //     query: e.target.search.value,
+  //     isLoading: true,
+  //     images: [],
+  //   });
+  //   this.fetchGallery(e.target.search.value, this.state.page);
+  // };
 
   onNextPage = () => {
     this.setState({
       page: this.state.page + 1,
-      isLoading: true,
     });
     this.fetchGallery(this.state.query, this.state.page + 1);
   };
@@ -58,26 +85,25 @@ export class App extends Component {
 
   async fetchGallery(query, page) {
     try {
+      this.setState({ loading: true });
       const response = await fetchHitsByQuery(query, page);
+      console.log('API Response:', response); 
       this.setState(prevState => {
         return {
           images: [...prevState.images, ...response],
+          showBtn: response.length >= 12
         };
       });
-      if (response.length < 12) {
-        this.setState({ showBtn: false });
-      }
-      if (response.length === 12) {
-        this.setState({ showBtn: true });
-      }
       if (response.length === 0) {
         Notify.failure('No matches found!');
       }
     } catch (error) {
+      console.error('API Error:', error);
       this.setState({ error });
     } finally {
       this.setState({ isLoading: false });
     }
+ 
   }
 
   render() {
@@ -85,7 +111,7 @@ export class App extends Component {
 
     return (
       <div className={s.App}>
-        <Searchbar onSubmit={this.onSubmit} />
+        <Searchbar changeQuery={this.changeQuery} />
         <ImageGallery images={images} onClickImage={this.onClickImage} />
         {isLoading && <Loader />}
         {showBtn && <Button onNextPage={this.onNextPage} />}
